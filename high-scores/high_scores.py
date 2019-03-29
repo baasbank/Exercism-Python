59f8c36f48c3342a8b4e32535ae49179bddabb84
Baasbank <baasb4real@yahoo.com>
2019-03-29 23:11:36 +0100

    high score manager :)

---
+++ high-scores/high_scores.py
@@ -0,0 +1,18 @@
+class HighScores(object):
+    def __init__(self, scores):
+        self.scores = scores
+
+    def personal_best(self):
+        return max(self.scores)
+
+    def latest(self):
+        return self.scores[-1]
+
+    def personal_top_three(self):
+        scores_length = len(self.scores)
+        sorted_scores = sorted(self.scores)
+        if scores_length == 1:
+            return sorted_scores
+        elif scores_length == 2:
+            return [sorted_scores[-1], sorted_scores[-2]]
+        return [sorted_scores[-1], sorted_scores[-2], sorted_scores[-3]]

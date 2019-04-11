758418b682486f0de1af67cdb83868780098ef14
Baasbank <baasb4real@yahoo.com>
2019-04-11 10:49:08 +0100

    improvements

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -9,6 +9,4 @@ class HighScores(object):
         return self.scores[-1]
 
     def personal_top_three(self):
-        scores_length = len(self.scores)
-        reverse_sorted_scores = sorted(self.scores, reverse=True)
-        return reverse_sorted_scores[:3] if scores_length >= 3 else reverse_sorted_scores[:scores_length]
+        return sorted(self.scores, reverse=True)[:3]
